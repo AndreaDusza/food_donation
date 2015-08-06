dda07d9964bdb775920b7339cdc559ec9b33cf26
malenesoeholm <malene.soeholm@gmail.com>
2015-08-06 21:13:37 +0100

    Login works (needs to be fitted with other code)

--- app/src/main/java/com/logcats/fooddonation/MainActivity.java
+++ app/src/main/java/com/logcats/fooddonation/MainActivity.java
@@ -3,10 +3,13 @@ package com.logcats.fooddonation;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -24,6 +27,15 @@ public class MainActivity extends Activity {
         prefs = this.getSharedPreferences(PREFERENCES_FILE_NAME, MODE_PRIVATE);
 
         greetUserAtFirstTime();
+
+        Button button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
