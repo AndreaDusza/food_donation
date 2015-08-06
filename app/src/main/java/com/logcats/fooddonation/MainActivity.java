5b54879a1136a302bcaecdffea354924e6250c24
malenesoeholm <malene.soeholm@gmail.com>
2015-08-07 00:01:52 +0100

    Merge branch 'dev' of https://github.com/AndreaDusza/food_donation into dev-login
    # Conflicts:
    #	.idea/workspace.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/logcats/fooddonation/DataManager.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/logcats/fooddonation/MainActivity.java
+++ app/src/main/java/com/logcats/fooddonation/MainActivity.java
@@ -3,10 +3,14 @@ package com.logcats.fooddonation;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
