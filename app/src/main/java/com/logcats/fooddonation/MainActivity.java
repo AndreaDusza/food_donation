ef09ee367045cf7e2190a40e928041daf46e6202
Stav Perle <perle.stav@gmail.com>
2015-08-07 02:46:23 +0100

    no message

--- app/src/main/java/com/logcats/fooddonation/MainActivity.java
+++ app/src/main/java/com/logcats/fooddonation/MainActivity.java
@@ -2,11 +2,19 @@ package com.logcats.fooddonation;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -56,14 +64,27 @@ public class MainActivity extends Activity {
     }
 
     private void showWelcomeScreen() {
-        new AlertDialog.Builder(this)
-                .setTitle(getString(R.string.welcome))
-                .setMessage(getString(R.string.tutorial_text))
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // continue with delete
-                    }
-                })
-                .show();
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.tutorial);
+       // dialog.setTitle(R.string.welcome);
+
+        // set the custom dialog components - text, image and button
+        TextView text = (TextView) dialog.findViewById(R.id.text);
+        text.setText("Tap on a marker to view the details of an offer");
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        // if button is clicked, close the custom dialog
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+
+        });
+
+        dialog.show();
     }
+
+
 }
