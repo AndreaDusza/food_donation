533cbec65798a01d5820bbfe8806ca53a2000f3b
filareta <filaretayordanova@gmail.com>
2015-08-07 09:12:08 +0100

    Send intent for opening a mail client

--- app/src/main/java/com/logcats/fooddonation/DetailedDonationActivity.java
+++ app/src/main/java/com/logcats/fooddonation/DetailedDonationActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -17,6 +20,7 @@ import java.text.SimpleDateFormat;
 
 public class DetailedDonationActivity extends Activity {
     public static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
+    public static final String MESSAGE_TYPE = "message/rfc822";
 
     private TextView mTitle;
     private ImageView mImageView;
@@ -24,6 +28,8 @@ public class DetailedDonationActivity extends Activity {
     private TextView mPostCreation;
     private TextView mDeactivationDate;
     private TextView mAvailabilityTime;
+    private Button mConnectButton;
+    private String recipient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +42,16 @@ public class DetailedDonationActivity extends Activity {
         mPostCreation = (TextView) findViewById(R.id.creationDate);
         mDeactivationDate = (TextView) findViewById(R.id.deactivationDate);
         mAvailabilityTime = (TextView) findViewById(R.id.availabilityTime);
+        mConnectButton = (Button) findViewById(R.id.connectButton);
+
+        DataManager dataManager = new DataManager(this);
 
         Intent intent = getIntent();
 
-        if(intent != null) {
+        if (intent != null) {
             Offer offer = (Offer) intent.getSerializableExtra(DonationListActivity.OFFER_EXTRA_KEY);
 
-            if(offer != null){
+            if (offer != null) {
                 mTitle.setText(offer.getTitle());
                 mDescription.setText(offer.getDescription());
 
@@ -51,7 +60,30 @@ public class DetailedDonationActivity extends Activity {
                 mDeactivationDate.setText(dateFormat.format(offer.getDeactivationDate()));
                 mAvailabilityTime.setText(offer.getAvailabilityTime());
                 PictureUtil.diplayPhoto(this, offer.getPicUrl(), mImageView);
+
+                User user = dataManager.getUserForOffer(offer);
+                if (user != null) {
+                    recipient = user.getEmail();
+                }
             }
         }
+
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType(MESSAGE_TYPE);
+
+                if (recipient != null) {
+                    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{recipient});
+                }
+
+                try {
+                    startActivity(intent);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(DetailedDonationActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
