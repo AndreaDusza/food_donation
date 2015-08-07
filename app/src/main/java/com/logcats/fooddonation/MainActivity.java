a9e70f925adbbfdac93005e43cfc38ac222788d2
Andrea Dusza <andidusza@gmail.com>
2015-08-07 04:21:20 +0100

    Sliding menu added to MainActivity with 4 menu buttons inside.

--- app/src/main/java/com/logcats/fooddonation/MainActivity.java
+++ app/src/main/java/com/logcats/fooddonation/MainActivity.java
@@ -1,19 +1,30 @@
 package com.logcats.fooddonation;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
 
     public static final String PREFERENCES_FILE_NAME = "MyAppPreferences";
     private static final String CLASS_NAME = "com.logcats.fooddonation.MainActivity";
@@ -29,38 +40,23 @@ public class MainActivity extends Activity {
 
         greetUserAtFirstTime();
 
-        Button button = (Button) findViewById(R.id.login);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UserLoginActivity.class);
-                startActivityForResult(intent, UserLoginActivity.ACTION_LOGIN);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        //noinspection SimplifiableIfStatement
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            //case R.id.action_logout:
-              //  return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Map view"),
+                        new PrimaryDrawerItem().withName("List view"),
+                        new PrimaryDrawerItem().withName("New donations"),
+                        new PrimaryDrawerItem().withName("Manage")
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        return true;
+                    }
+                })
+                .build();
     }
 
     private void greetUserAtFirstTime(){
